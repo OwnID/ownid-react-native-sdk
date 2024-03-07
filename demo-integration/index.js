d98650dcc061b5452cede98d742096f5ba19270d
Dmytro Kryvoruchko <dkrivoruchko@gmail.com>
2024-03-07 18:47:46 +0200

    1

--- demo-integration/index.js
+++ demo-integration/index.js
@@ -2,14 +2,14 @@ import { AppRegistry } from 'react-native';
 import App from './src';
 
 import Auth from './auth.service';
-const auth = new Auth("https://node-mongo.custom.demo.dev.ownid.com/api/auth/");
+const auth = new Auth("...");
 
 import OwnId from '@ownid/react-native-core';
 
 const appName = "OwnIDReactNativeIntegrationDemo";
 
 AppRegistry.registerRunnable(appName, async (initialProps) => {
-    await OwnId.init({ appId: "d1yk6gcngrc0og", env: "dev" }, "OwnIDIntegration/3.1.0");
+    await OwnId.init({ appId: "..." }, "OwnIDIntegration/3.1.0");
 
     AppRegistry.registerComponent(appName, () => App(auth));
     AppRegistry.runApplication(appName, initialProps);
